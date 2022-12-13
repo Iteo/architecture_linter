5c3113bdea03ee37eeb06346b126b881f5084f81
lukasz.wojcik <lukasz.wojcik@iteo.com>
2022-12-13 10:32:20 +0100

    Downgrade version fix

--- test/analyzers/mocks/imports_mock/domain/domain_class.dart
+++ test/analyzers/mocks/imports_mock/domain/domain_class.dart
@@ -1,7 +1,9 @@
 import '../data/data_class.dart';
+import '../data/data_class_2.dart';
 import '../presentation/presentation_class.dart';
 
 class DomainClass {
   final PresentationClass presentationClass = PresentationClass();
   final DataClass dataClass = DataClass();
+  final DataClass2 dataClass2 = DataClass2();
 }
