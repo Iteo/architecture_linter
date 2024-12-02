75547b2784821c3e1243d88fd247e5a4686f697b
tomkad99 <tomasz.kadziolka@iteo.com>
2024-12-02 13:51:04 +0100

    fix: two extensions tests

--- test/utils/import_directive_utils_test.dart
+++ test/utils/import_directive_utils_test.dart
@@ -1,5 +1,6 @@
 import 'package:architecture_linter/src/configuration/layer.dart';
 import 'package:architecture_linter/src/configuration/layers_config.dart';
+import 'package:architecture_linter/src/configuration/lint_severity.dart';
 import 'package:architecture_linter/src/utils/import_directive_utils.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
@@ -71,190 +72,151 @@ void main() {
     });
   });
 
-  // group('getConfigFromLastInPath', () {
-  //   test('Test if returns null for empty list of layer configurations', () {
-  //     final emptyList = <LayerConfig>[];
-  //
-  //     final result = TestImportDirective().getConfigFromLastInPath(emptyList);
-  //
-  //     expect(result, null);
-  //   });
-  //
-  //   test('Test if returns null for null import', () {
-  //     final domainConfig = <LayerConfig>[
-  //       LayerConfig(
-  //         severity: LintSeverity.info,
-  //         layer: Layer('Domain', 'domain'),
-  //       ),
-  //     ];
-  //     final testImport = TestImportDirective();
-  //     final testUri = TestStringLiteral();
-  //     when(() => testUri.stringValue).thenReturn(null);
-  //     when(() => testImport.uri).thenReturn(testUri);
-  //
-  //     final result = testImport.getConfigFromLastInPath(domainConfig);
-  //
-  //     expect(result, null);
-  //   });
-  //
-  //   test('Test if returns null for empty layer path', () {
-  //     final blankConfig = <LayerConfig>[
-  //       LayerConfig(
-  //         severity: LintSeverity.info,
-  //         layer: Layer('', ''),
-  //       ),
-  //     ];
-  //     final testImport = TestImportDirective();
-  //     final testUri = TestStringLiteral();
-  //     when(() => testUri.stringValue).thenReturn('domain');
-  //     when(() => testImport.uri).thenReturn(testUri);
-  //
-  //     final result = testImport.getConfigFromLastInPath(blankConfig);
-  //
-  //     expect(result, null);
-  //   });
-  //
-  //   test('Test if returns null for import path that is not banned', () {
-  //     final infrastructureConfig = <LayerConfig>[
-  //       LayerConfig(
-  //         severity: LintSeverity.info,
-  //         layer: Layer('Infra', 'infrastructure'),
-  //       ),
-  //     ];
-  //     final testImport = TestImportDirective();
-  //     final testUri = TestStringLiteral();
-  //     when(() => testUri.stringValue).thenReturn('domain');
-  //     when(() => testImport.uri).thenReturn(testUri);
-  //
-  //     final result = testImport.getConfigFromLastInPath(
-  //       infrastructureConfig,
-  //     );
-  //
-  //     expect(result, null);
-  //   });
-  //
-  //   test('Test if returns a config for a single layer', () {
-  //     final domainConfig = <LayerConfig>[
-  //       LayerConfig(
-  //         severity: LintSeverity.info,
-  //         layer: Layer('Domain', 'domain'),
-  //       ),
-  //     ];
-  //     final testImport = TestImportDirective();
-  //     final testUri = TestStringLiteral();
-  //     when(() => testUri.stringValue).thenReturn('domain');
-  //     when(() => testImport.uri).thenReturn(testUri);
-  //
-  //     final result = testImport.getConfigFromLastInPath(domainConfig);
-  //
-  //     expect(result, const TypeMatcher<LayerConfig>());
-  //   });
-  //
-  //   test('Test if returns a config for the last one in path', () {
-  //     final domainConfig = <LayerConfig>[
-  //       LayerConfig(
-  //         severity: LintSeverity.info,
-  //         layer: Layer('Domain', '(domain)'),
-  //       ),
-  //       LayerConfig(
-  //         severity: LintSeverity.info,
-  //         layer: Layer('Use Case', '(use_case)'),
-  //       ),
-  //     ];
-  //     final testImport = TestImportDirective();
-  //     final testUri = TestStringLiteral();
-  //     when(() => testUri.stringValue).thenReturn('/domain/use_case/feature/');
-  //     when(() => testImport.uri).thenReturn(testUri);
-  //
-  //     final result = testImport.getConfigFromLastInPath(domainConfig);
-  //
-  //     expect(
-  //       result,
-  //       LayerConfig(
-  //         severity: LintSeverity.info,
-  //         layer: Layer('Use Case', '(use_case)'),
-  //       ),
-  //     );
-  //   });
-  // });
-  //
-  // group('isRelative', () {
-  //   test('Returns false If the path is null', () {
-  //     final testImport = TestImportDirective();
-  //     final testUri = TestStringLiteral();
-  //     when(() => testUri.stringValue).thenReturn(null);
-  //     when(() => testImport.uri).thenReturn(testUri);
-  //
-  //     final result = testImport.isRelative;
-  //
-  //     expect(result, false);
-  //   });
-  //
-  //   test('Returns false If the path is empty', () {
-  //     final testImport = TestImportDirective();
-  //     final testUri = TestStringLiteral();
-  //     when(() => testUri.stringValue).thenReturn('');
-  //     when(() => testImport.uri).thenReturn(testUri);
-  //
-  //     final result = testImport.isRelative;
-  //
-  //     expect(result, false);
-  //   });
-  //
-  //   test('Returns false If the path is an absolute variant', () {
-  //     final testImport = TestImportDirective();
-  //     final testUri = TestStringLiteral();
-  //     when(() => testUri.stringValue).thenReturn('package:test/test.dart');
-  //     when(() => testImport.uri).thenReturn(testUri);
-  //
-  //     final result = testImport.isRelative;
-  //
-  //     expect(result, false);
-  //   });
-  //
-  //   test('Returns true If path is exact file name', () {
-  //     final testImport = TestImportDirective();
-  //     final testUri = TestStringLiteral();
-  //     when(() => testUri.stringValue).thenReturn('some_file.dart');
-  //     when(() => testImport.uri).thenReturn(testUri);
-  //
-  //     final result = testImport.isRelative;
-  //
-  //     expect(result, true);
-  //   });
-  //
-  //   test('Returns true If path is a file in an upper catalog', () {
-  //     final testImport = TestImportDirective();
-  //     final testUri = TestStringLiteral();
-  //     when(() => testUri.stringValue).thenReturn('../some_file.dart');
-  //     when(() => testImport.uri).thenReturn(testUri);
-  //
-  //     final result = testImport.isRelative;
-  //
-  //     expect(result, true);
-  //   });
-  //
-  //   test('Returns true If path is a file in an nested catalog catalog', () {
-  //     final testImport = TestImportDirective();
-  //     final testUri = TestStringLiteral();
-  //     when(() => testUri.stringValue).thenReturn('../../some_file.dart');
-  //     when(() => testImport.uri).thenReturn(testUri);
-  //
-  //     final result = testImport.isRelative;
-  //
-  //     expect(result, true);
-  //   });
-  // });
-  //
+  group('getConfigFromLastInPath', () {
+    const utils = ImportDirectiveUtils;
+
+    test('Test if returns null for empty list of layer configurations', () {
+      final noConfig = <LayerConfig>[];
+
+      final result = ImportDirectiveUtils.getConfigFromLastInPath(
+        noConfig,
+        'domain',
+      );
+
+      expect(result, null);
+    });
+
+    test('Test if returns null for null import', () {
+      final domainConfig = <LayerConfig>[
+        LayerConfig(
+          severity: LintSeverity.info,
+          layer: Layer('Domain', 'domain'),
+        ),
+      ];
+
+      final result = ImportDirectiveUtils.getConfigFromLastInPath(
+        domainConfig,
+        null,
+      );
+
+      expect(result, null);
+    });
+
+    test('Test if returns null for empty layer path', () {
+      final blankConfig = <LayerConfig>[
+        LayerConfig(
+          severity: LintSeverity.info,
+          layer: Layer('', ''),
+        ),
+      ];
+
+      final result = ImportDirectiveUtils.getConfigFromLastInPath(
+        blankConfig,
+        'domain',
+      );
+
+      expect(result, null);
+    });
+
+    test('Test if returns null for import path that is not banned', () {
+      final infrastructureConfig = <LayerConfig>[
+        LayerConfig(
+          severity: LintSeverity.info,
+          layer: Layer('Infra', 'infrastructure'),
+        ),
+      ];
+
+      final result = ImportDirectiveUtils.getConfigFromLastInPath(
+        infrastructureConfig,
+        'domain',
+      );
+
+      expect(result, null);
+    });
+
+    test('Test if returns a config for a single layer', () {
+      final domainConfig = <LayerConfig>[
+        LayerConfig(
+          severity: LintSeverity.info,
+          layer: Layer('Domain', 'domain'),
+        ),
+      ];
+
+      final result = ImportDirectiveUtils.getConfigFromLastInPath(
+        domainConfig,
+        'domain',
+      );
+
+      expect(result, const TypeMatcher<LayerConfig>());
+    });
+
+    test('Test if returns a config for the last one in path', () {
+      final domainConfig = <LayerConfig>[
+        LayerConfig(
+          severity: LintSeverity.info,
+          layer: Layer('Domain', '(domain)'),
+        ),
+        LayerConfig(
+          severity: LintSeverity.info,
+          layer: Layer('Use Case', '(use_case)'),
+        ),
+      ];
+
+      final result = ImportDirectiveUtils.getConfigFromLastInPath(
+        domainConfig,
+        '/domain/use_case/feature/',
+      );
+
+      expect(
+        result,
+        LayerConfig(
+          severity: LintSeverity.info,
+          layer: Layer('Use Case', '(use_case)'),
+        ),
+      );
+    });
+  });
+
+  group('isRelative', () {
+    test('Returns false If the path is null', () {
+      final result = ImportDirectiveUtils.isRelative(null);
+      expect(result, false);
+    });
+
+    test('Returns false If the path is empty', () {
+      final result = ImportDirectiveUtils.isRelative('');
+      expect(result, false);
+    });
+
+    test('Returns false If the path is an absolute variant', () {
+      final result = ImportDirectiveUtils.isRelative('package:test/test.dart');
+      expect(result, false);
+    });
+
+    test('Returns true If path is exact file name', () {
+      final result = ImportDirectiveUtils.isRelative('test.dart');
+      expect(result, true);
+    });
+
+    test('Returns true If path is a file in an upper catalog', () {
+      final result = ImportDirectiveUtils.isRelative('../test.dart');
+      expect(result, true);
+    });
+
+    test('Returns true If path is a file in an nested catalog catalog', () {
+      final result = ImportDirectiveUtils.isRelative('../../test.dart');
+      expect(result, true);
+    });
+
   // group('existsInBannedLayers', () {
   //   test('Returns false If layer list is empty', () {
-  //     final emptyList = <Layer>{};
+  //     final noConfig = <Layer>{};
   //     final testImport = TestImportDirective();
   //     final testUri = TestStringLiteral();
   //     when(() => testUri.stringValue).thenReturn('path');
   //     when(() => testImport.uri).thenReturn(testUri);
   //
-  //     final result = testImport.existsInBannedLayers('', emptyList);
+  //     final result = testImport.existsInBannedLayers('', noConfig);
   //
   //     expect(result, false);
   //   });
@@ -341,4 +303,5 @@ void main() {
   //     expect(result, true);
   //   });
   // });
+});
 }
